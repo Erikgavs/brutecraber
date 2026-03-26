edc0849a0780b6fa501be1ad4045b105b0d787da
Erik <erikgavs@gmail.com>
2026-03-26 11:44:51 +0100

    auto-detect module created

---
+++ src/detector.rs
@@ -0,0 +1,9 @@
+pub fn detect(hash: &str) -> &str {
+    match hash.len() {
+        32 => "md5",
+        40 => "sha1",
+        64 => "sha156",
+        128 => "sha512",
+        _ => "hash not recognized, try using -t parameter",
+    }
+}
