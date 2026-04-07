76fa98329b52dc056f1cd7a2eb93c6989b9e3043
Erik <erikgavs@gmail.com>
2026-04-07 18:59:57 +0200

    dead code removed

--- src/cracker.rs
+++ src/cracker.rs
@@ -410,7 +410,6 @@ pub fn run(hashes: &[&str], wordlist: &str, hash_type: &str, rule: bool) -> usiz
     wordlist.lines().par_bridge().for_each(|word| {
         bar.inc(1);
 
-        // if rules enabled, generate variants; otherwise just the original word
         let words_to_try = if rule {
             crate::rules::apply(word)
         } else {
@@ -445,141 +444,12 @@ pub fn run(hashes: &[&str], wordlist: &str, hash_type: &str, rule: bool) -> usiz
                         }
                     }
                 }
-                "md5-salt" => {
-                    for h in hashes {
-                        if let Some((salt, target)) = h.split_once(':') {
-                            let hash = hashes::md5::crack_with_salt(w, salt);
-                            if hash == target {
-                                bar.println(format!(
-                                    "{} hash cracked [salt:{}] {} -> {}",
-                                    star.green(),
-                                    salt,
-                                    target,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
-                "sha1-salt" => {
-                    for h in hashes {
-                        if let Some((salt, target)) = h.split_once(':') {
-                            let hash = hashes::sha1_hash::crack_with_salt(w, salt);
-                            if hash == target {
-                                bar.println(format!(
-                                    "{} hash cracked [salt:{}] {} -> {}",
-                                    star.green(),
-                                    salt,
-                                    target,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
-                "sha256-salt" => {
-                    for h in hashes {
-                        if let Some((salt, target)) = h.split_once(':') {
-                            let hash = hashes::sha256::crack_with_salt(w, salt);
-                            if hash == target {
-                                bar.println(format!(
-                                    "{} hash cracked [salt:{}] {} -> {}",
-                                    star.green(),
-                                    salt,
-                                    target,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
-                "sha512-salt" => {
-                    for h in hashes {
-                        if let Some((salt, target)) = h.split_once(':') {
-                            let hash = hashes::sha512::crack_with_salt(w, salt);
-                            if hash == target {
-                                bar.println(format!(
-                                    "{} hash cracked [salt:{}] {} -> {}",
-                                    star.green(),
-                                    salt,
-                                    target,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
-                "sha3-256-salt" => {
-                    for h in hashes {
-                        if let Some((salt, target)) = h.split_once(':') {
-                            let hash = hashes::sha3_256::crack_with_salt(w, salt);
-                            if hash == target {
-                                bar.println(format!(
-                                    "{} hash cracked [salt:{}] {} -> {}",
-                                    star.green(),
-                                    salt,
-                                    target,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
-                "sha3-512-base64" => {
-                    let hash = hashes::sha3_512::crack(w);
-                    for h in hashes {
-                        if let Ok(decoded) = general_purpose::STANDARD.decode(h) {
-                            let hex: String =
-                                decoded.iter().map(|m| format!("{:02x}", m)).collect();
-
-                            if hex == hash {
-                                bar.println(format!(
-                                    "{} hash decoded and cracked {} -> {} -> {}",
-                                    star.green(),
-                                    h,
-                                    hex,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
-                "sha3-512-salt" => {
-                    for h in hashes {
-                        if let Some((salt, target)) = h.split_once(':') {
-                            let hash = hashes::sha3_512::crack_with_salt(w, salt);
-                            if hash == target {
-                                bar.println(format!(
-                                    "{} hash cracked [salt:{}] {} -> {}",
-                                    star.green(),
-                                    salt,
-                                    target,
-                                    w
-                                ));
-                                found.fetch_add(1, Ordering::Relaxed);
-                            }
-                        }
-                    }
-                }
                 "bcrypt" => {
                     for h in hashes {
                         if hashes::bcrypt::crack(w, h) {
                             bar.println(format!("{} hash cracked {} -> {}", star.green(), h, w));
                             found.fetch_add(1, Ordering::Relaxed);
                         }
-                    }
-                }
-                "ntlm" => {
-                    let hash = hashes::ntlm::crack(w);
-                    if hashes.contains(&hash.as_str()) {
-                        bar.println(format!("{} hash cracked {} -> {}", star.green(), hash, w));
-                        found.fetch_add(1, Ordering::Relaxed);
                     }
                 }
                 _ => {
