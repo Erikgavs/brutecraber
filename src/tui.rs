e5b082f1a713d2f2624f54d90ae2a5775f9290f5
Erik <erikgavs@gmail.com>
2026-05-06 13:32:15 +0200

    rui.rs created

---
+++ src/tui.rs
@@ -0,0 +1,12 @@
+use std::io;
+use std::time::Duration;
+
+use crossterm::event::{self, Event, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{
+    EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+};
+use ratatui::Terminal;
+use ratatui::backend::CrosstermBackend;
+
+pub fn run() -> io::Result<()> {}
